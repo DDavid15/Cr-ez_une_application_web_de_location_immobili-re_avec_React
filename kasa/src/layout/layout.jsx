ea4232bf97602d75c05871783069491b92030d77
David Duribreux <duribreuxdavid15@gmail.com>
2025-04-11 13:23:41 +0200

    Finalisation des pages Home, A Propos et 404 - version desktop et passe mobile

--- kasa/src/layout/layout.jsx
+++ kasa/src/layout/layout.jsx
@@ -1,23 +1,25 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import "../styles/layout/_layout.scss";
 
 function Layout() {
   const location = useLocation();
   const path = location.pathname;
 
-  let mainClass = "main";
+  // Classe spécifique selon la route
+  let pageClass = "";
 
-  if (path === "/") mainClass = "home";
-  else if (path === "/a-propos") mainClass = "apropos";
-  else if (path.startsWith("/logement/")) mainClass = "logement";
-  else mainClass = "default";
+  if (path === "/") pageClass = "home";
+  else if (path === "/a-propos") pageClass = "apropos";
+  else if (path.startsWith("/logement/")) pageClass = "logement";
+  else pageClass = "notfound";
 
   return (
     <>
       <Header />
-      <main className={mainClass}>
-        <Outlet /> {/* ← C'est ce qui permet d'afficher tes pages */}
+      <main className={`page ${pageClass}`}>
+        <Outlet />
       </main>
       <Footer />
     </>
--- kasa/src/pages/NotFound.jsx
+++ kasa/src/pages/NotFound.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import "../styles/pages/_notfound.scss";
+
 function NotFound() {
-  return <h2>Non Trouvé</h2>;
+  return (
+    <>
+      <h1 className="notfound__code">404</h1>
+      <p className="notfound__message">
+        Oups! La page que vous demandez n’existe pas.
+      </p>
+      <Link to="/" className="notfound__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </>
+  );
 }
 
 export default NotFound;
