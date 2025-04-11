0984592cee0041c7afaf16186b43e808b055eaa9
David Duribreux <duribreuxdavid15@gmail.com>
2025-04-11 13:58:07 +0200

    Déploiement GitHub Pages avec configuration base pour Vite

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/layout";
+
+import Home from "./pages/Home";
+import Logement from "./pages/Logement";
+import APropos from "./pages/APropos";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/a-propos" element={<APropos />} />
+        <Route path="/logement/:id" element={<Logement />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo-header.svg"; // à adapter selon le nom de ton fichier
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logo} alt="Kasa" className="header__logo" />
+      <nav className="header__nav">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/a-propos"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          A Propos
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
